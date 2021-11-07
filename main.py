4fbe182e6791a76cd7712fb947eaa2fa21d98940
wilkiechrisj <wilkiechrisj@gmail.com>
2021-11-07 13:42:57 -0600

    working on validating args - this time with new keys hidden

--- main.py
+++ main.py
@@ -1,11 +1,58 @@
 import praw
 import pandas
-from flask import Flask
+from flask import Flask, request
+from prawcore import NotFound
 
 
 APP = Flask(__name__)
-reddit = praw.Reddit(client_id='2QxECe_51Rkj5TXyv0RCPA', client_secret='tVmK3MuwFarRCQGTFNiebzxO9uSTTg',
-                     user_agent='RedScraper')
+reddit = praw.Reddit()
+
+NOT_FOUND = {'ERROR': 'SUBREDDIT DOES NOT EXIST!'}
+VAR_ERROR = {'ERROR': 'YOUR VARIABLES ARE INCORRECT - PLEASE SEE README AT urlhere'}
+
+TIMEFRAME = ['hour', 'day', 'week', 'month', 'year', 'all']
+
+
+def subreddit(name, num):
+    pass
+
+
+def exists(name):
+
+    try:
+        reddit.subreddits.search_by_name(name, exact=True)
+    except NotFound:
+        return False
+    return True
+
+
+def validate_args(name, num, time):
+
+    if name:
+        if exists(name):
+            pass
+        else:
+            name = False
+    else:
+        name = False
+
+    if num:
+        if num.isdigit():
+            num = int(num)
+        else:
+            num = False
+    else:
+        num = 10
+
+    if time:
+        if time in TIMEFRAME:
+            pass
+        else:
+            time = False
+    else:
+        pass
+
+    return name, num, time
 
 
 @APP.route('/', methods=['GET'])
@@ -13,18 +60,37 @@ def root():
     return 'Hello World!'
 
 
+@APP.route('/sub', methods=['GET'])
+def user_sub():
+
+    name = request.args.get('subreddit')
+    num = request.args.get('limit')
+    time = request.args.get('time')
+
+    validate_args(name, num, time)
+
+    sub = reddit.subreddit(name).top
+
+
 @APP.route('/all', methods=['GET'])
 def r_all():
 
-    sub = reddit.subreddit('tifu').hot(limit=10)
+    sub = reddit.subreddit('all').hot(limit=10)
     posts = []
 
     for post in sub:
-        posts.append([post.title, post.score, post.url, post.selftext])
+        posts.append([post.title, post.url, post.selftext])
 
-    posts = pandas.DataFrame(posts, columns=['TITLE', 'SCORE', 'URL', 'BODY'])
+    posts = pandas.DataFrame(posts, columns=['TITLE', 'URL', 'BODY'])
 
     return posts.to_html()
+
+
+@APP.route('/test', methods=['GET'])
+def test():
+    var = request.args.get('var')
+    print(type(var))
+    return var
 
 
 if __name__ == '__main__':
